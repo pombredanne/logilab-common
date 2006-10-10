a2d2653a73873aaa3d5965ab2cfe04c38b3018a3
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2006-10-10 12:42:09 +0200

    make pytest browse recursively the current directory to find tests

---
+++ logservice.py
@@ -0,0 +1,30 @@
+"""log utilities
+
+Copyright (c) 2003-2004 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+http://www.logilab.fr/ -- mailto:contact@logilab.fr
+"""
+
+__revision__ = "$Id: logservice.py,v 1.5 2006-03-05 16:13:28 syt Exp $"
+
+from logilab.common.logger import make_logger, LOG_ERR, LOG_WARN, LOG_NOTICE, \
+     LOG_INFO, LOG_CRIT, LOG_DEBUG
+
+def init_log(treshold, method='eprint', sid='common-log-service',
+             logger=None, output=None):
+    """init the logging system and and log methods to builtins"""
+    if logger is None:
+        logger = make_logger(method, treshold, sid, output=output)
+    # add log functions and constants to builtins
+    __builtins__.update({'log': logger.log,
+                         'log_traceback' : logger.log_traceback,
+                         'LOG_CRIT':   LOG_CRIT,
+                         'LOG_ERR':    LOG_ERR,
+                         'LOG_WARN':   LOG_WARN,
+                         'LOG_NOTICE': LOG_NOTICE,
+                         'LOG_INFO' :  LOG_INFO,
+                         'LOG_DEBUG':  LOG_DEBUG,
+                         })
+
+init_log(LOG_ERR)
+
+
