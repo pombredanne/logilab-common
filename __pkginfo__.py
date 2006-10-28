ef91dc0dccf1db02731db46e44854d7b70c7136b
Sylvain <syt@logilab.fr>
2006-10-28 17:24:10 +0200

    packaging fix

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -15,7 +15,8 @@
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 """logilab.common packaging information"""
 
-modname = 'logilab-common'
+distname = 'logilab-common'
+modname = 'common'
 numversion = (0, 20, 0)
 version = '.'.join([str(num) for num in numversion])
 
