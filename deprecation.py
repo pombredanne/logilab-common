fb5e654a8c6a478e7d43ea7c35adca67fb04f8b1
Sylvain <syt@logilab.fr>
2007-09-21 18:37:12 +0200

    copyright

--- deprecation.py
+++ deprecation.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2005-2006 LOGILAB S.A. (Paris, FRANCE).
+# Copyright (c) 2005-2007 LOGILAB S.A. (Paris, FRANCE).
 # http://www.logilab.fr/ -- mailto:contact@logilab.fr
 
 # This program is free software; you can redistribute it and/or modify it under
