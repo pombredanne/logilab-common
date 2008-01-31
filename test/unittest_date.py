18fed17b56401f473fa6c8ca8c8eead07fa54c0d
Adrien Di Mascio <Adrien.DiMascio@logilab.fr>
2008-01-31 18:04:11 +0100

    little function that adds a number of days worked to a date

--- test/unittest_date.py
+++ test/unittest_date.py
@@ -8,10 +8,10 @@ from logilab.common.date import date_range
 
 try:
     from mx.DateTime import Date, RelativeDate
-    from logilab.common.date import endOfMonth
+    from logilab.common.date import endOfMonth, add_days_worked
 except ImportError:
     from datetime import date as Date
-    endOfMonth = None
+    endOfMonth = add_days_worked = None
 
 class DateTC(TestCase):
     
@@ -34,6 +34,20 @@ class DateTC(TestCase):
         r = list(date_range(Date(2000,11,30), Date(2001,2,3), endOfMonth))
         expected = [Date(2000,11,30), Date(2000,12,31), Date(2001,1,31)]
         self.assertListEquals(r, expected)
+
+    def test_add_days_worked(self):
+        if add_days_worked is None:
+            self.skip('mx.DateTime is not installed')
+        add = add_days_worked
+        # normal
+        self.assertEquals(add(Date(2008, 1, 3), 1), Date(2008, 1, 4))
+        # skip week-end
+        self.assertEquals(add(Date(2008, 1, 3), 2), Date(2008, 1, 7))
+        # skip 2 week-ends
+        self.assertEquals(add(Date(2008, 1, 3), 8), Date(2008, 1, 15))
+        # skip holiday + week-end
+        self.assertEquals(add(Date(2008, 4, 30), 2), Date(2008, 5, 4))
+
 
     
 if __name__ == '__main__':
