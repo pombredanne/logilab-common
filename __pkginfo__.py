3745dbdc657287a7af700e32d418e04c7dc42b11
Emile Anclin <emile.anclin@logilab.fr>
2008-09-10 14:48:14 +0200

    prepare version

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -8,7 +8,7 @@ __docformat__ = "restructuredtext en"
 
 distname = 'logilab-common'
 modname = 'common'
-numversion = (0, 34, 0)
+numversion = (0, 35, 0)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'GPL'
