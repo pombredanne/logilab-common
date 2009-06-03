d41dd7d9825ec4ce57209f9d598cd2d7f5de1bdc
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2009-06-03 18:05:53 +0200

    prepare 0.41.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -8,7 +8,7 @@ __docformat__ = "restructuredtext en"
 
 distname = 'logilab-common'
 modname = 'common'
-numversion = (0, 40, 1)
+numversion = (0, 41, 0)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'GPL'
