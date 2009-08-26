a5cc275ddf9f548145ed298573eec38684cb70c7
Olivier Cayrol (Logilab) <Olivier.Cayrol@logilab.fr>
2009-08-26 16:14:48 +0200

    Enhanced package description

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -27,12 +27,13 @@ modules,
   * writing interactive command line tools
   * manipulation files and character strings
   * interfacing to OmniORB
-  * generating of SQL queries
+  * generating SQL queries
   * running unit tests
   * manipulating tree structures
   * accessing RDBMS (currently postgreSQL, mysql and sqlite)
   * generating text and HTML reports
   * logging
+  * parsing XML processing instructions
   * more...
 """
 
