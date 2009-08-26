bd58b9b6469a41258c1d6d05cf191a6124619281
Olivier Cayrol (Logilab) <Olivier.Cayrol@logilab.fr>
2009-08-26 17:35:31 +0200

    Preparing version 0.45.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -8,7 +8,7 @@ __docformat__ = "restructuredtext en"
 
 distname = 'logilab-common'
 modname = 'common'
-numversion = (0, 44, 0)
+numversion = (0, 45, 0)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'GPL'
