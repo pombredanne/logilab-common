a1be483591e72352a1dbf5089a7b1ea150bfb84b
Emile Anclin <emile.anclin@logilab.fr>
2010-10-06 12:39:20 +0200

    remove py2.2 compat ; use deprecation warnings

--- testlib.py
+++ testlib.py
@@ -84,8 +84,7 @@ except ImportError:
     test_support = TestSupport()
 
 # pylint: disable=W0622
-from logilab.common.compat import (set, enumerate, any, sorted, InheritableSet,
-                                   callable)
+from logilab.common.compat import set, any, sorted, InheritableSet, callable
 # pylint: enable-msg=W0622
 from logilab.common.modutils import load_module_from_name
 from logilab.common.debugger import Debugger, colorize_source
