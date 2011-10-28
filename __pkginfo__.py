d7f4fb158080c943bea47fc437c5a17a3d9ab50f
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2011-10-28 11:59:41 +0200

    0.57.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -24,7 +24,7 @@ modname = 'common'
 subpackage_of = 'logilab'
 subpackage_master = True
 
-numversion = (0, 57, 0)
+numversion = (0, 57, 1)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'LGPL' # 2.1 or later
