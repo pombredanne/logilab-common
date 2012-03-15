092fa728c7bf11d40dcee2df7f4c1c597a87d153
Alain Leufroy <alain.leufroy@logilab.fr>
2012-03-15 13:37:49 +0100

    [shellutil] add argument to ``ProgressBar.update`` to tune cursor progression (closes #88981)

--- test/unittest_shellutils.py
+++ test/unittest_shellutils.py
@@ -144,6 +144,30 @@ class ProgressBarTC(TestCase):
     def test_overflow(self):
         self._update_test(5, (8, 16, 25, 33, 42, (42, True)), size=42)
 
+    def test_update_exact(self):
+        pgb_stream = StringIO()
+        expected_stream = StringIO()
+        size=20
+        pgb = ProgressBar(100, size, stream=pgb_stream)
+        last = 0
+        for dots in xrange(10, 105, 15):
+            pgb.update(dots, exact=True)
+            dots /= 5
+            expected_stream.write("\r["+('.'*dots)+(' '*(size-dots))+"]")
+            self.assertEqual(pgb_stream.getvalue(), expected_stream.getvalue())
+        
+    def test_update_relative(self):
+        pgb_stream = StringIO()
+        expected_stream = StringIO()
+        size=20
+        pgb = ProgressBar(100, size, stream=pgb_stream)
+        last = 0
+        for dots in xrange(5, 105, 5):
+            pgb.update(5, exact=False)
+            dots /= 5
+            expected_stream.write("\r["+('.'*dots)+(' '*(size-dots))+"]")
+            self.assertEqual(pgb_stream.getvalue(), expected_stream.getvalue())
+
 
 class AcquireLockTC(TestCase):
 
