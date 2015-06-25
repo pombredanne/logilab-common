7a1cadcf4a73ad62120d7466fa91647db5cccac4
Julien Cristau <julien.cristau@logilab.fr>
2015-06-25 19:50:20 +0200

    [pkg] Prepare 1.0.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -25,7 +25,7 @@ modname = 'common'
 subpackage_of = 'logilab'
 subpackage_master = True
 
-numversion = (0, 63, 2)
+numversion = (1, 0, 0)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'LGPL' # 2.1 or later
