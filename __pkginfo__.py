4f6514eff4436e48678d27d145c90b86726ee29b
David Douard <david.douard@logilab.fr>
2016-05-27 13:34:59 +0200

    [pkg] 1.2.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -25,7 +25,7 @@ modname = 'common'
 subpackage_of = 'logilab'
 subpackage_master = True
 
-numversion = (1, 2, 0)
+numversion = (1, 2, 1)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'LGPL' # 2.1 or later
