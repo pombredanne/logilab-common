56d7078d1b7802ad00bb717c682990f68fed2a66
Denis Laxalde <denis.laxalde@logilab.fr>
2016-11-18 11:47:57 +0100

    [pkg] Version 1.3.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -25,7 +25,7 @@ modname = 'common'
 subpackage_of = 'logilab'
 subpackage_master = True
 
-numversion = (1, 2, 2)
+numversion = (1, 3, 0)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'LGPL' # 2.1 or later
